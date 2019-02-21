8767b8fbe495ca5af9311aa0c6c9152c605ef12f
Olga <revina.volga@gmail.ru>
2019-02-21 17:46:07 +0300

    add task LoveTriangles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var sum = 0;
+    for (var i = 0; i < preferences.length; i++) {
+      for (var j = 0; j < preferences.length; j++) {
+        for (var k = 0; k < preferences.length; k++) {
+          if (preferences[i] == j + 1 && preferences[j] == k + 1 && preferences[k] == i + 1) {
+            sum++;
+          }
+        }
+      }
+    }
+    return Math.floor(sum / 3);
 };
